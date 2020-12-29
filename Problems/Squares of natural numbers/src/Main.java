879a9d9794e1909926e100970512db15898ddc19
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-12-29 02:17:13 +0100

    Add solved problem 'Squares of natural numbers'

--- Problems/Squares of natural numbers/src/Main.java
+++ Problems/Squares of natural numbers/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int squareNum;
+
+        for (int i = 1; i <= sqrt(num); i++) {
+            squareNum = i * i;
+            System.out.println(squareNum);
+        }
     }
 }
