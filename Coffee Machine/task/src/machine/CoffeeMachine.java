93c858a2a5c5a5b104763aa0619001304d9900c2
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-12-29 03:12:45 +0100

    Add solved problem 'Coffee Machine Stage 2/6: Machines have needs'

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,35 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsOfCoffeeNeeded = sc.nextInt();
+        System.out.println("For " + cupsOfCoffeeNeeded + " cups of coffee you will need:");
+        calculateIngredients(cupsOfCoffeeNeeded);
+    }
+
+    public static void calculateIngredients(int cupsOfCoffeeNeeded) {
+        System.out.println(waterNeeded(cupsOfCoffeeNeeded) + " ml of water");
+        System.out.println(milkNeeded(cupsOfCoffeeNeeded) + " ml of milk");
+        System.out.println(beansNeeded(cupsOfCoffeeNeeded) + " g of coffee beans");
+    }
+
+    public static int waterNeeded(int cupsOfCoffeeNeeded) {
+        return cupsOfCoffeeNeeded * 200;
+    }
+
+    public static int milkNeeded(int cupsOfCoffeeNeeded) {
+        return cupsOfCoffeeNeeded * 50;
+    }
+
+    public static int beansNeeded(int cupsOfCoffeeNeeded) {
+        return cupsOfCoffeeNeeded * 15;
+    }
+
+    public static void printCoffeeMachineMainFunctions() {
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
